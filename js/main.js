1514bfbfc2a4ae12a4f163d0f6b2c5bc3c98d5e6
Artem Korolev <artem256korolev@gmail.com>
2025-07-09 20:49:07 +0300

    fix slider

--- js/main.js
+++ js/main.js
@@ -76,6 +76,35 @@ const popup = () => {
 popup()
 
 $(document).ready(function(){
+    $('.feedback__content_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 932,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function(){
     $('.inside__content_slider').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
